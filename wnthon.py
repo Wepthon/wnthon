e6675af397722d4e6fa9afdf8241668dc64ed95f
Wepthon <138310014+Wepthon@users.noreply.github.com>
2023-08-04 13:54:38 +0300

    wnthon.py

--- wnthon.py
+++ wnthon.py
@@ -96,9 +96,7 @@ async def spam_function(event, sandy, cat, sleeptimem, sleeptimet, DelaySpam=Fal
 @wnthon.on(events.NewMessage(outgoing=True, pattern=r"\.الاوامر"))
 async def _(event):
     await event.edit(commands)
-await event.client.send_file(
-                    event.chat_id,
-                    "https://t.me/l_raqq/5",
+
 @wnthon.on(events.NewMessage(outgoing=True, pattern=r"\.فحص"))
 async def _(event):
     start = datetime.datetime.now()
